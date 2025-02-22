2dac9d126dcd310b73f0f1fd2458bc918696a056
madsnbkristensen <madskristensen@hotmail.dk>
2025-02-22 23:36:28 +0100

    making site responsive

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -186,7 +186,7 @@ const CarouselItem = React.forwardRef<
       aria-roledescription="slide"
       className={cn(
         "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "-ml-0 md:-ml-4" : "pt-4",
+        orientation === "horizontal" ? "sm:-ml-0 md:-ml-4" : "pt-4",
         className,
       )}
       {...props}
